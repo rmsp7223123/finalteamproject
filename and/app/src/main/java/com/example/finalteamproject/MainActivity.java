961732c7b64f8bd2f8e69af57387481d20b6a9fc
sbn121 <129023074+sbn121@users.noreply.github.com>
2023-07-25 09:20:38 +0900

    fragment

--- and/app/src/main/java/com/example/finalteamproject/MainActivity.java
+++ and/app/src/main/java/com/example/finalteamproject/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.finalteamproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
@@ -17,6 +18,23 @@ public class MainActivity extends AppCompatActivity {
         binding.bottomNavigationView.setSelectedItemId(R.id.fab);
         binding.fltbtnHome.setOnClickListener(v->{
             binding.bottomNavigationView.setSelectedItemId(R.id.fab);
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, new MainFragment()).commit();
         });
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if(item.getItemId()==R.id.chat){
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, new ChatFragment()).commit();
+            }else if(item.getItemId()==R.id.gps){
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, new GpsFramgent()).commit();
+            }else if(item.getItemId()==R.id.game){
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, new GameFragment()).commit();
+            }else if(item.getItemId()==R.id.setting){
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, new SettingFragment()).commit();
+            }else {
+                return false;
+            }
+            return true;
+        });
+
     }
 }
