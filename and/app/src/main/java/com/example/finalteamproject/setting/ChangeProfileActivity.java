8dd1d96cca0c4011817a7f2564d9da9a8eabed05
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-07-26 19:58:03 +0900

    액티비티별 액션바 숨기기 추가

--- and/app/src/main/java/com/example/finalteamproject/setting/ChangeProfileActivity.java
+++ and/app/src/main/java/com/example/finalteamproject/setting/ChangeProfileActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.finalteamproject.HideActionBar;
 import com.example.finalteamproject.R;
 import com.example.finalteamproject.databinding.ActivityChangeProfileBinding;
 
@@ -16,5 +17,6 @@ public class ChangeProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityChangeProfileBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        new HideActionBar().hideActionBar(this);
     }
 }
