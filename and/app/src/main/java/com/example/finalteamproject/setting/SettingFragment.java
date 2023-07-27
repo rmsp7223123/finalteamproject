da8e8d47cdc926c0efb6f79767cf41ee674fff46
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-07-27 10:48:36 +0900

    세팅 xml 추가 및 수정, forward 벡터이미지 추가

--- and/app/src/main/java/com/example/finalteamproject/setting/SettingFragment.java
+++ and/app/src/main/java/com/example/finalteamproject/setting/SettingFragment.java
@@ -1,5 +1,6 @@
 package com.example.finalteamproject.setting;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -18,6 +19,10 @@ public class SettingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentSettingBinding.inflate(inflater,container,false);
+        binding.containerLinearChangeProfile.setOnClickListener(v -> {
+            Intent intent = new Intent(getContext(), ChangeProfileActivity.class);
+            startActivity(intent);
+        });
         return binding.getRoot();
     }
 }
