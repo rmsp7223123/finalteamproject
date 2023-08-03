a75a38d13884f0e8b5296c54b888b850f7a20c7f
615637 <129474590+615637@users.noreply.github.com>
2023-08-03 15:06:26 +0900

    내 위치 표시
    - 내 위치 추적
    - 추적 된 위도, 경도 로그 찍힘
    - 내 위치 마커

--- and/app/src/main/java/com/example/finalteamproject/gps/GpsFragment.java
+++ and/app/src/main/java/com/example/finalteamproject/gps/GpsFragment.java
@@ -1,12 +1,13 @@
 package com.example.finalteamproject.gps;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -14,6 +15,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,24 +27,20 @@ import com.naver.maps.geometry.LatLng;
 import com.naver.maps.map.CameraUpdate;
 import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.MapView;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
-import com.naver.maps.map.UiSettings;
 import com.naver.maps.map.overlay.Marker;
 import com.naver.maps.map.util.FusedLocationSource;
 
 //지도 나오는 첫 페이지
 public class GpsFragment extends Fragment implements OnMapReadyCallback {
 
-    private static final int LOCATION_PERMISSION_REQUEST_CODE = 100;
-    private static final String[] PERMISSIONS = {
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION
-    };
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
 
     private FusedLocationSource locationSource;
     private NaverMap naverMap;
-
+    private double lat, lon;
     FragmentGpsBinding binding;
 
     @Override
@@ -61,6 +59,7 @@ public class GpsFragment extends Fragment implements OnMapReadyCallback {
         locationSource =
                 new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
 
+        //화면
         binding.recvGps.setAdapter(new GpsAdapter(getContext()));
         binding.recvGps.setLayoutManager(new LinearLayoutManager(getContext()));
 
@@ -73,39 +72,39 @@ public class GpsFragment extends Fragment implements OnMapReadyCallback {
     }
 
     @Override
-    public void onMapReady(@NonNull NaverMap naverMap) {
-
-        // NaverMap 객체 받아서 NaverMap 객체에 위치 소스 지정
-        this.naverMap = naverMap;
-        naverMap.setLocationSource(locationSource);
-
-        // 권한 확인, 결과는 onRequestPermissionResult 콜백 메서드 호출
-        ActivityCompat.requestPermissions(requireActivity(), PERMISSIONS, LOCATION_PERMISSION_REQUEST_CODE);
-
-        Marker marker = new Marker();
-        marker.setPosition(new LatLng(37.5670135, 126.9783740));
-        marker.setMap(naverMap);
-
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,  @NonNull int[] grantResults) {
+        if (locationSource.onRequestPermissionsResult(
+                requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated()) { // 권한 거부됨
+                naverMap.setLocationTrackingMode(LocationTrackingMode.None);
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(
+                requestCode, permissions, grantResults);
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        // request code와 권한 획득 여부 확인
-        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                if (naverMap != null) {
-                    naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        this.naverMap = naverMap;
+        naverMap.setLocationSource(locationSource); //내 위치
+        naverMap.setLocationTrackingMode(LocationTrackingMode.Follow); //위치 추적 모드
 
-                }
-            } else {
-                // 위치 권한이 거부된 경우
+        //내 위치 위도, 경도 로그
+        naverMap.addOnLocationChangeListener(new NaverMap.OnLocationChangeListener() {
+            @Override
+            public void onLocationChange(@NonNull Location location) {
+                lat = location.getLatitude();
+                lon = location.getLongitude();
+                // 현재 위치에 마커 생성
+                Marker marker = new Marker();
+                marker.setPosition(new LatLng(lat, lon));
+                marker.setMap(naverMap);
+
+                Log.d("위치", "위도, 경도: "+lat+", "+lon);
             }
-        }
+        });
     }
-
-
-
-
-
 }
+
