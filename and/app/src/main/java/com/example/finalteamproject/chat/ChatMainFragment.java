ca1f519dd0303dc0751e00e345942ea393c8661e
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-08-03 11:30:07 +0900

    대화 플로팅버튼 나오는 부분 수정

--- and/app/src/main/java/com/example/finalteamproject/chat/ChatMainFragment.java
+++ and/app/src/main/java/com/example/finalteamproject/chat/ChatMainFragment.java
@@ -22,11 +22,41 @@ public class ChatMainFragment extends Fragment {
     FragmentManager manager;
 
     Fragment fragment = null;
+
+    boolean isAllFabVisible;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentChatBinding.inflate(inflater, container, false);
         manager = getActivity().getSupportFragmentManager();
+        binding.fltbtnCall.setVisibility(View.GONE);
+        binding.fltbtnFriendList.setVisibility(View.GONE);
+        binding.fltbtnMessage.setVisibility(View.GONE);
+        binding.tvCall.setVisibility(View.GONE);
+        binding.tvFriendList.setVisibility(View.GONE);
+        binding.tvMessage.setVisibility(View.GONE);
+        isAllFabVisible = false;
+        binding.fltbtnMenu.setOnClickListener(v -> {
+            if(!isAllFabVisible) {
+                binding.fltbtnMessage.show();
+                binding.fltbtnCall.show();
+                binding.fltbtnFriendList.show();
+                binding.tvCall.setVisibility(View.VISIBLE);
+                binding.tvFriendList.setVisibility(View.VISIBLE);
+                binding.tvMessage.setVisibility(View.VISIBLE);
+                binding.fltbtnMenu.setImageResource(R.drawable.baseline_close_24);
+                isAllFabVisible = true;
+            } else {
+                binding.fltbtnMessage.hide();
+                binding.fltbtnCall.hide();
+                binding.fltbtnFriendList.hide();
+                binding.tvCall.setVisibility(View.GONE);
+                binding.tvFriendList.setVisibility(View.GONE);
+                binding.tvMessage.setVisibility(View.GONE);
+                binding.fltbtnMenu.setImageResource(R.drawable.baseline_add_24);
+                isAllFabVisible = false;
+            }
+        });
         return binding.getRoot();
     }
 
