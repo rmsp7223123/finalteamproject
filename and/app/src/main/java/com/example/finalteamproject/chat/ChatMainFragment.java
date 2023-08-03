e489285a8f28c4ca4a107bf06c6f7300c1d65c5a
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-08-03 11:21:19 +0900

    대화 플로팅버튼 수정

--- and/app/src/main/java/com/example/finalteamproject/chat/ChatMainFragment.java
+++ and/app/src/main/java/com/example/finalteamproject/chat/ChatMainFragment.java
@@ -19,7 +19,6 @@ import com.google.android.material.navigation.NavigationBarView;
 
 public class ChatMainFragment extends Fragment {
     FragmentChatBinding binding;
-    private boolean isRailVisible = true;
     FragmentManager manager;
 
     Fragment fragment = null;
@@ -28,86 +27,8 @@ public class ChatMainFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentChatBinding.inflate(inflater, container, false);
         manager = getActivity().getSupportFragmentManager();
-        setupNavigationRail();
-        binding.navigationRail.setSelectedItemId(R.id.friend_list);
-        binding.containerLinearOpen.setOnClickListener(view -> {
-            binding.containerLinearOpen.setVisibility(View.INVISIBLE);
-            showNavigationRail();
-            binding.navigationRail.bringToFront();
-        });
-//        binding.navigationRail.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-//            @Override
-//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//                Fragment fragment = null;
-//                if(item.getItemId() == R.id.friend_list) {
-//                    fragment = new FriendListFragment();
-//                } else {
-//                    return false;
-//                }
-//                manager.beginTransaction().replace(R.id.container_frame_call_msg, fragment).commit();
-//                return true;
-//            }
-//        });
         return binding.getRoot();
-
     }
 
-    private void setupNavigationRail() {
-        binding.navigationRail.setOnItemSelectedListener(item -> {
-            if (item.getItemId() == R.id.close) {
-                binding.containerLinearOpen.setVisibility(View.VISIBLE);
-                hideNavigationRail();
-            } else if(item.getItemId() == R.id.friend_list){
-                fragment = new FriendListFragment();
-            } else if (item.getItemId() == R.id.message) {
-                fragment = new MessageFragment();
-            }else {
-                showNavigationRail();
-            }
-            manager.beginTransaction().replace(R.id.container_frame_call_msg,fragment).commit();
-            binding.navigationRail.bringToFront();
-            return true;
-        });
-    }
 
-    private void hideNavigationRail() {
-        if (isRailVisible) {
-            Animation slideOut = AnimationUtils.loadAnimation(requireContext(), R.anim.slide_out_to_left);
-            slideOut.setAnimationListener(new Animation.AnimationListener() {
-                @Override
-                public void onAnimationStart(Animation animation) {}
-
-                @Override
-                public void onAnimationEnd(Animation animation) {
-                    binding.navigationRail.setVisibility(View.GONE);
-                    isRailVisible = false;
-                }
-
-                @Override
-                public void onAnimationRepeat(Animation animation) {}
-            });
-            binding.navigationRail.startAnimation(slideOut);
-        }
-    }
-
-    private void showNavigationRail() {
-        if (!isRailVisible) {
-            Animation slideIn = AnimationUtils.loadAnimation(requireContext(), R.anim.slide_in_from_left);
-            slideIn.setAnimationListener(new Animation.AnimationListener() {
-                @Override
-                public void onAnimationStart(Animation animation) {
-                    binding.navigationRail.setVisibility(View.VISIBLE);
-                }
-
-                @Override
-                public void onAnimationEnd(Animation animation) {
-                    isRailVisible = true;
-                }
-
-                @Override
-                public void onAnimationRepeat(Animation animation) {}
-            });
-            binding.navigationRail.startAnimation(slideIn);
-        }
-    }
 }
