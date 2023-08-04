de26926d77f6ceb117903b5f7aafa0cc7a53d60c
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-08-04 09:42:48 +0900

    프로필변경 닉네임 변경 기능 추가

--- and/app/src/main/java/com/example/finalteamproject/setting/ChangeProfileActivity.java
+++ and/app/src/main/java/com/example/finalteamproject/setting/ChangeProfileActivity.java
@@ -15,11 +15,14 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.finalteamproject.HideActionBar;
 import com.example.finalteamproject.R;
+import com.example.finalteamproject.common.CommonConn;
 import com.example.finalteamproject.common.RetrofitClient;
 import com.example.finalteamproject.common.RetrofitInterface;
 import com.example.finalteamproject.databinding.ActivityChangeProfileBinding;
@@ -54,17 +57,39 @@ public class ChangeProfileActivity extends AppCompatActivity {
         binding.btnChangeProfile.setOnClickListener(v -> {
             showDialog();
         });
+        binding.btnChangeNickname.setOnClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("닉네임 변경");
+            final View customLayout = getLayoutInflater().inflate(R.layout.dialog_change_nickname, null);
+            builder.setView(customLayout);
+            builder.setPositiveButton("취소", (dialog, which) -> {
+            });
+            builder.setNegativeButton("확인", (dialog, which) -> {
+                EditText edt_nickname = customLayout.findViewById(R.id.edt_nickname);
+                // 중복확인 검사 추가하기
+                if(edt_nickname.length() > 0 ) {
+                    sendDialogDataToActivity(edt_nickname.getText().toString());
+                    binding.tvNickname.setText(edt_nickname.getText().toString());
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        });
     }
 
-    public void showDialog(){
+    private void sendDialogDataToActivity(String data) {
+        Toast.makeText(this, data + "으로 변경되었습니다.", Toast.LENGTH_SHORT).show();
+    }
+
+    public void showDialog() {
         String[] dialog_item = {"갤러리", "카메라"};
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("사진 업로드 방식");
         builder.setSingleChoiceItems(dialog_item, -1, (dialog, i) -> {
-            if(dialog_item[i].equals("갤러리")){
+            if (dialog_item[i].equals("갤러리")) {
                 //갤러리 로직
                 showGallery();
-            }else if(dialog_item[i].equals("카메라")){
+            } else if (dialog_item[i].equals("카메라")) {
                 //카메라 로직
                 showCamera();
             }
@@ -74,7 +99,7 @@ public class ChangeProfileActivity extends AppCompatActivity {
         dialog.show();
     }
 
-    public void showGallery(){
+    public void showGallery() {
         Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_PICK);
@@ -83,6 +108,7 @@ public class ChangeProfileActivity extends AppCompatActivity {
     }
 
     Uri camera_uri = null; // 사진 uri
+
     public void showCamera() {
         //ContentReseolver(). 앱 --> 컨텐트 리졸버(작업자) ---> 미디어 저장소
         camera_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, new ContentValues());
@@ -100,7 +126,7 @@ public class ChangeProfileActivity extends AppCompatActivity {
                 // 액티비티(카메라)가 종료 되면 콜백으로 데이터를 받는 부분 기존(onActivityResult메소드)가 실행 되었고 현재는 해당 메소드
                 Glide.with(ChangeProfileActivity.this).load(camera_uri).into(binding.imgvProfileimg);
                 File file = new File(getRealPath(camera_uri));
-                if(file!= null) {
+                if (file != null) {
                     RequestBody fileBody = RequestBody.create(MediaType.parse("image/jpeg"), file);
                     MultipartBody.Part filePart = MultipartBody.Part.createFormData("file", "test.jpg", fileBody);
                     RetrofitInterface api = new RetrofitClient().retrofitLogin().create(RetrofitInterface.class);
@@ -121,25 +147,25 @@ public class ChangeProfileActivity extends AppCompatActivity {
         });
     }
 
-    public String getRealPath(Uri contentUri){
+    public String getRealPath(Uri contentUri) {
         String res = null;
         String[] proj = {MediaStore.Images.Media.DATA};//
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             Cursor cursor = getContentResolver().query(contentUri, proj, null, null);
-            if(cursor.moveToFirst()){
+            if (cursor.moveToFirst()) {
                 int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
                 res = cursor.getString(column_index);
             }
             cursor.close();//다썼으니까 닫음.
         }
-        Log.d("TAG", "getRealPath: 커서"+res);
+        Log.d("TAG", "getRealPath: 커서" + res);
         return res;
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==REQ_GALLERY && resultCode ==RESULT_OK){
+        if (requestCode == REQ_GALLERY && resultCode == RESULT_OK) {
             Glide.with(this).load(data.getData()).into(binding.imgvProfileimg);
             String img_path = getRealPath(data.getData());
 
