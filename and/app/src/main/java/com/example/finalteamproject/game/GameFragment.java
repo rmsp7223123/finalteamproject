f0c73a23ae03c20439733328fc0c63fec9f342c7
615637 <129474590+615637@users.noreply.github.com>
2023-08-04 11:24:52 +0900

    game code copy
    기억력 게임 코드 복붙함.
    디자인, 점수 부분 변경 예정

--- and/app/src/main/java/com/example/finalteamproject/game/GameFragment.java
+++ and/app/src/main/java/com/example/finalteamproject/game/GameFragment.java
@@ -1,5 +1,6 @@
 package com.example.finalteamproject.game;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,7 +9,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.finalteamproject.R;
 import com.example.finalteamproject.databinding.FragmentGameBinding;
 
 public class GameFragment extends Fragment {
@@ -19,6 +19,14 @@ public class GameFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentGameBinding.inflate(inflater, container, false);
+
+        binding.btnGameStart.setOnClickListener(v -> {
+            Intent intent = new Intent(getContext(), GameActivity.class);
+            startActivity(intent);
+        });
+
+
+
         return binding.getRoot();
     }
 }
