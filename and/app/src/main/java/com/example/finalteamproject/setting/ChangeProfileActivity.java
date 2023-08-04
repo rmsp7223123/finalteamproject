0408deda9e3729cc17de9e53c045cbb78ab7561a
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-08-04 09:14:18 +0900

    프로필 이미지 변경 기능 추가

--- and/app/src/main/java/com/example/finalteamproject/setting/ChangeProfileActivity.java
+++ and/app/src/main/java/com/example/finalteamproject/setting/ChangeProfileActivity.java
@@ -1,16 +1,46 @@
 package com.example.finalteamproject.setting;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.finalteamproject.HideActionBar;
 import com.example.finalteamproject.R;
+import com.example.finalteamproject.common.RetrofitClient;
+import com.example.finalteamproject.common.RetrofitInterface;
 import com.example.finalteamproject.databinding.ActivityChangeProfileBinding;
+
+import java.io.File;
+import java.util.HashMap;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ChangeProfileActivity extends AppCompatActivity {
 
     ActivityChangeProfileBinding binding;
+
+    private final int REQ_GALLERY = 1000;
+
+    ActivityResultLauncher<Intent> launcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +51,114 @@ public class ChangeProfileActivity extends AppCompatActivity {
         binding.imgvBack.setOnClickListener(v -> {
             finish();
         });
+        binding.btnChangeProfile.setOnClickListener(v -> {
+            showDialog();
+        });
     }
+
+    public void showDialog(){
+        String[] dialog_item = {"갤러리", "카메라"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("사진 업로드 방식");
+        builder.setSingleChoiceItems(dialog_item, -1, (dialog, i) -> {
+            if(dialog_item[i].equals("갤러리")){
+                //갤러리 로직
+                showGallery();
+            }else if(dialog_item[i].equals("카메라")){
+                //카메라 로직
+                showCamera();
+            }
+            dialog.dismiss();
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    public void showGallery(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_PICK);
+//        startActivity(intent); //단순 실행 결과를 알수가없다.
+        startActivityForResult(intent, REQ_GALLERY);
+    }
+
+    Uri camera_uri = null; // 사진 uri
+    public void showCamera() {
+        //ContentReseolver(). 앱 --> 컨텐트 리졸버(작업자) ---> 미디어 저장소
+        camera_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, new ContentValues());
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, camera_uri);
+        launcher.launch(cameraIntent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                // 액티비티(카메라)가 종료 되면 콜백으로 데이터를 받는 부분 기존(onActivityResult메소드)가 실행 되었고 현재는 해당 메소드
+                Glide.with(ChangeProfileActivity.this).load(camera_uri).into(binding.imgvProfileimg);
+                File file = new File(getRealPath(camera_uri));
+                if(file!= null) {
+                    RequestBody fileBody = RequestBody.create(MediaType.parse("image/jpeg"), file);
+                    MultipartBody.Part filePart = MultipartBody.Part.createFormData("file", "test.jpg", fileBody);
+                    RetrofitInterface api = new RetrofitClient().retrofitLogin().create(RetrofitInterface.class);
+                    api.clientSendFile("file.f", new HashMap<>(), filePart).enqueue(new Callback<String>() {
+                        @Override
+                        public void onResponse(Call<String> call, Response<String> response) {
+
+                        }
+
+                        @Override
+                        public void onFailure(Call<String> call, Throwable t) {
+
+                        }
+                    });
+                }
+
+            }
+        });
+    }
+
+    public String getRealPath(Uri contentUri){
+        String res = null;
+        String[] proj = {MediaStore.Images.Media.DATA};//
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            Cursor cursor = getContentResolver().query(contentUri, proj, null, null);
+            if(cursor.moveToFirst()){
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                res = cursor.getString(column_index);
+            }
+            cursor.close();//다썼으니까 닫음.
+        }
+        Log.d("TAG", "getRealPath: 커서"+res);
+        return res;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==REQ_GALLERY && resultCode ==RESULT_OK){
+            Glide.with(this).load(data.getData()).into(binding.imgvProfileimg);
+            String img_path = getRealPath(data.getData());
+
+            //MultiPart 형태로 전송 (File)
+            RequestBody fileBody = RequestBody.create(MediaType.parse("image/jpeg"), new File(img_path));
+            MultipartBody.Part filePart = MultipartBody.Part.createFormData("file", "test.jpg", fileBody);
+            RetrofitInterface api = new RetrofitClient().retrofitLogin().create(RetrofitInterface.class);
+            api.clientSendFile("file.f", new HashMap<>(), filePart).enqueue(new Callback<String>() {
+                @Override
+                public void onResponse(Call<String> call, Response<String> response) {
+
+                }
+
+                @Override
+                public void onFailure(Call<String> call, Throwable t) {
+
+                }
+            });
+        }
+    }
+
 }
