5e5910ae2a7e304f615e502a6e2708983e8eeb77
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-08-07 16:53:14 +0900

    ---

--- and/app/src/main/java/com/example/finalteamproject/FirebaseMessageReceiver.java
+++ and/app/src/main/java/com/example/finalteamproject/FirebaseMessageReceiver.java
@@ -33,34 +33,6 @@ public class FirebaseMessageReceiver extends FirebaseMessagingService {
     private final String TAG = "확인용";
     int uniqueRandomValue = new Random().nextInt();
 
-    // 메세지 받는 부분
-    @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
-
-        // Check if message contains a data payload.
-        if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-
-            if (/* Check if data needs to be processed by long running job */ true) {
-                // For long-running tasks (10 seconds or more) use WorkManager.
-                scheduleJob();
-            } else {
-                // Handle message within 10 seconds
-                handleNow();
-            }
-
-        }
-
-        // Check if message contains a notification payload.
-        if (remoteMessage.getNotification() != null) {
-            sendNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
-        }
-
-    }
-
     @Override
     public void onNewToken(@NonNull String token) {
         Log.d(TAG, "Refreshed token: " + token);
@@ -88,53 +60,117 @@ public class FirebaseMessageReceiver extends FirebaseMessagingService {
         // TODO: Implement this method to send token to your app server.
         Log.d(TAG, token);
     }
-    //Notification모양을 변경할수있음.
-    private void sendNotification(String title, String message) {
-        Intent intent = new Intent(this, MainActivity.class);
+    // Override onMessageReceived() method to extract the
+    // title and
+    // body from the message passed in FCM
+    @Override
+    public void
+    onMessageReceived(RemoteMessage remoteMessage)
+    {
+        // First case when notifications are received via
+        // data event
+        // Here, 'title' and 'message' are the assumed names
+        // of JSON
+        // attributes. Since here we do not have any data
+        // payload, This section is commented out. It is
+        // here only for reference purposes.
+        /*if(remoteMessage.getData().size()>0){
+            showNotification(remoteMessage.getData().get("title"),
+                          remoteMessage.getData().get("message"));
+        }*/
+
+        // Second case when notification payload is
+        // received.
+        if (remoteMessage.getNotification() != null) {
+            // Since the notification is received directly
+            // from FCM, the title and the body can be
+            // fetched directly as below.
+            showNotification(
+                    remoteMessage.getNotification().getTitle(),
+                    remoteMessage.getNotification().getBody());
+        }
+    }
+
+    // Method to get the custom Design for the display of
+    // notification.
+    private RemoteViews getCustomDesign(String title,
+                                        String message)
+    {
+        RemoteViews remoteViews = new RemoteViews(
+                getApplicationContext().getPackageName(),
+                R.layout.notification);
+        remoteViews.setTextViewText(R.id.title, title);
+        remoteViews.setTextViewText(R.id.message, message);
+        remoteViews.setImageViewResource(R.id.icon,
+                R.drawable.haerin2);
+        return remoteViews;
+    }
+
+    // Method to display the notifications
+    public void showNotification(String title,
+                                 String message)
+    {
+        // Pass the intent to switch to the MainActivity
+        Intent intent
+                = new Intent(this, MainActivity.class);
+        // Assign channel ID
+        String channel_id = "notification_channel";
+        // Here FLAG_ACTIVITY_CLEAR_TOP flag is set to clear
+        // the activities present in the activity stack,
+        // on the top of the Activity that is to be launched
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
-                PendingIntent.FLAG_ONE_SHOT |PendingIntent.FLAG_IMMUTABLE);
+        // Pass the intent to PendingIntent to start the
+        // next Activity
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                this, 0, intent, PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_IMMUTABLE
+        );
 
-        String channelId = "fcm_default_channel";
-        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        RemoteViews notificationLayout = new RemoteViews(getPackageName(), R.layout.notification);
-        notificationLayout.setTextViewText(R.id.title, title);
-        notificationLayout.setTextViewText(R.id.message, message);
-        try {
-            Bitmap bitmap = Glide.with(getApplicationContext())
-                    .asBitmap()
-                    .load(R.drawable.haerin2)
-                    .circleCrop()
-                    .submit(512, 512)
-                    .get();
-            notificationLayout.setImageViewBitmap(R.id.icon, bitmap);
-        } catch (ExecutionException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        // Create a Builder object using NotificationCompat
+        // class. This will allow control over all the flags
+        NotificationCompat.Builder builder
+                = new NotificationCompat
+                .Builder(getApplicationContext(),
+                channel_id)
+                .setSmallIcon(R.drawable.haerin2)
+                .setAutoCancel(true)
+                .setVibrate(new long[] { 1000, 1000, 1000,
+                        1000, 1000 })
+                .setOnlyAlertOnce(true)
+                .setContentIntent(pendingIntent);
+
+        // A customized design for the notification can be
+        // set only for Android versions 4.1 and above. Thus
+        // condition for the same is checked here.
+        if (Build.VERSION.SDK_INT
+                >= Build.VERSION_CODES.JELLY_BEAN) {
+            builder = builder.setContent(
+                    getCustomDesign(title, message));
+        } // If Android Version is lower than Jelly Beans,
+        // customized layout cannot be used and thus the
+        // layout is set as follows
+        else {
+            builder = builder.setContentTitle(title)
+                    .setContentText(message)
+                    .setSmallIcon(R.drawable.haerin2);
         }
-        NotificationCompat.Builder notificationBuilder =
-                new NotificationCompat.Builder(this, channelId)
-                        .setAutoCancel(true)
-                        .setOnlyAlertOnce(true)
-                        .setSmallIcon(R.drawable.baseline_mail_outline_24)
-                        .setSound(defaultSoundUri)
-                        .setContentIntent(pendingIntent)
-                        .setCustomContentView(notificationLayout);
-
-
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        // Since android Oreo notification ch annel is needed.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(channelId,
-                    getString(R.string.project_id),
+        // Create an object of NotificationManager class to
+        // notify the
+        // user of events that happen in the background.
+        NotificationManager notificationManager
+                = (NotificationManager)getSystemService(
+                Context.NOTIFICATION_SERVICE);
+        // Check if the Android Version is greater than Oreo
+        if (Build.VERSION.SDK_INT
+                >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel
+                    = new NotificationChannel(
+                    channel_id, "web_app",
                     NotificationManager.IMPORTANCE_HIGH);
-            notificationManager.createNotificationChannel(channel);
+            notificationManager.createNotificationChannel(
+                    notificationChannel);
         }
 
-        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+        notificationManager.notify(uniqueRandomValue, builder.build());
     }
 
     public static class MyWorker extends Worker {
