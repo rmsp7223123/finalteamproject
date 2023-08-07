29ed60cd205de160671777948251ae463645b3fb
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-08-07 15:56:52 +0900

    fcm 수정

--- and/app/src/main/java/com/example/finalteamproject/chat/MessageChatActivity.java
+++ and/app/src/main/java/com/example/finalteamproject/chat/MessageChatActivity.java
@@ -5,8 +5,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 
-import com.example.finalteamproject.FirebaseMessageReceiver;
-import com.example.finalteamproject.R;
 import com.example.finalteamproject.databinding.ActivityMessageChatBinding;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -25,8 +23,8 @@ public class MessageChatActivity extends AppCompatActivity {
 
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     private DatabaseReference databaseReference = firebaseDatabase.getReference();
-
     public static String messageId = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,8 +56,6 @@ public class MessageChatActivity extends AppCompatActivity {
                 MessageDTO temp = new MessageDTO(messageDTO.getImgRes(),messageDTO.getNickname(),messageText,currentTime,true);
                 // 파이어베이스 경로를 닉네임이 아닌 id로 바꾸기
                 databaseReference.child("chat").child(messageDTO.getNickname()).child(messageId).setValue(temp);
-                FirebaseMessageReceiver fmr = new FirebaseMessageReceiver();
-                fmr.showNotification(binding.tvNickname.getText().toString(), binding.edtMessage.getText().toString());
                 adapter.notifyDataSetChanged();
                 binding.edtMessage.setText("");
             }
