2b3649ce6f3dbcdb00acd1bcee8bad09ae9afea0
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-08-09 19:23:51 +0900

    a
    a

--- and/app/src/main/java/com/example/finalteamproject/main/BoardMainAdapter.java
+++ and/app/src/main/java/com/example/finalteamproject/main/BoardMainAdapter.java
@@ -1,12 +1,14 @@
 package com.example.finalteamproject.main;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
@@ -19,8 +21,6 @@ public class BoardMainAdapter extends RecyclerView.Adapter<BoardMainAdapter.View
 
     ArrayList<BoardMainDTO> list;
     MainActivity activity;
-    public BoardMainAdapter(ArrayList<BoardMainDTO> list, Activity activity) {
-    public BoardMainAdapter(ArrayList<BoardMainDTO> list, Activity activity) {
     Fragment fragment;
     String align;
 
@@ -28,6 +28,7 @@ public class BoardMainAdapter extends RecyclerView.Adapter<BoardMainAdapter.View
         this.fragment = fragment;
         this.list = list;
         this.activity = (MainActivity) activity;
+        this.align = align;
     }
     @NonNull
     @Override
@@ -43,14 +44,7 @@ public class BoardMainAdapter extends RecyclerView.Adapter<BoardMainAdapter.View
         h.binding.tvBoardName.setText(list.get(i).getTv_board_name());
         h.binding.imgvMove.setImageResource(list.get(i).getImgv_move());
         h.binding.lnBoardSelect.setOnClickListener(v -> {
-            activity.changeFragment(list.get(i).getTv_board_name());
-
-            activity.changeFragment(list.get(i).getTv_board_name());
-
             activity.changeFragment(fragment, list.get(i).getTv_board_name(), align, activity);
-
-            activity.changeFragment(fragment, list.get(i).getTv_board_name(), align, activity);
-
         });
     }
 
