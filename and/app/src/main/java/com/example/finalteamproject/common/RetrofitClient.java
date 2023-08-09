2ba27cb1dbe8d60bee94f53f911c5e6b676483c1
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-08-09 10:03:59 +0900

    레트로핏 수정

--- and/app/src/main/java/com/example/finalteamproject/common/RetrofitClient.java
+++ and/app/src/main/java/com/example/finalteamproject/common/RetrofitClient.java
@@ -2,19 +2,12 @@ package com.example.finalteamproject.common;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
+import retrofit2.http.HEAD;
 
 public class RetrofitClient {
     public Retrofit retrofitLogin(){
         return new Retrofit.Builder()
-<<<<<<< HEAD
                 .baseUrl("http://192.168.0.87:8080/cloud/")
-=======
-<<<<<<< HEAD
-                .baseUrl("http://192.168.0.87:8080/cloud/")
-=======
-                .baseUrl("http://192.168.0.33:8080/cloud/")
->>>>>>> bitna
->>>>>>> temp_main
                 .addConverterFactory(ScalarsConverterFactory.create())
                 .build();
     }
