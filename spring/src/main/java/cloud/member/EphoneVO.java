6647ec32e293baf1e4cf3c21ca1e2b426deb348e
sbn121 <129023074+sbn121@users.noreply.github.com>
2023-08-10 10:00:26 +0900

    11

--- spring/src/main/java/cloud/member/EphoneVO.java
+++
@@ -1,9 +0,0 @@
-package cloud.member;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter @Setter
-public class EphoneVO {
-	String member_id, ephone_name, ephone_phone, ephone_relation, ephone_phone_id;
-}
