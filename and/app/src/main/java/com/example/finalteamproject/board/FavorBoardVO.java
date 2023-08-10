ab09a064d620660186b277a16910f65bf1714924
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-08-10 10:17:58 +0900

    00

--- and/app/src/main/java/com/example/finalteamproject/board/FavorBoardVO.java
+++
@@ -1,7 +0,0 @@
-package com.example.finalteamproject.board;
-
-
-public class FavorBoardVO {
-	int fav_board_id, favor, fav_board_writecount;
-	String writer, fav_board_title, fav_board_content, fav_board_writedate, fav_board_img;
-}
