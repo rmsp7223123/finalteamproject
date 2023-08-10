89ade51b2ce344042355e58b7abfb68db2d15750
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-08-10 09:59:44 +0900

    a
    sa

--- and/app/src/main/java/com/example/finalteamproject/chat/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.example.finalteamproject.chat;
-
-public class Constants {
-    public static final char[] INITIAL_SOUND = {
-            'ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'
-    };
-}
