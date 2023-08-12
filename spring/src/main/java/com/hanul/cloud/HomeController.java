4c25615b28be01a271e0a395c269cb72dbfa2d83
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-08-12 17:35:32 +0900

    dbㅅㅜㅈㅓㅇ

--- spring/src/main/java/com/hanul/cloud/HomeController.java
+++ spring/src/main/java/com/hanul/cloud/HomeController.java
@@ -30,13 +30,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 	
-//	@Autowired @Qualifier("test") SqlSession sql;
-//	
-//	@RequestMapping("test")
-//	public String test() {
-//		sql.selectOne("customer.mapper.test");
-//		return "a";
-//	}
+	@Autowired @Qualifier("project") SqlSession sql;
+	
+	@RequestMapping("test")
+	public String test() {
+	 sql.selectOne("customer.mapper.test");
+		return "a";
+	}
 //	
 //	
 //	@RequestMapping("/")
