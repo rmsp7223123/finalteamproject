5d81cca96f1e2220056f40a8c26cba5e7b5ebfc0
615637 <129474590+615637@users.noreply.github.com>
2023-08-16 18:01:54 +0900

    게임스코어 update조건

--- 게임 스코어 조회.sql
+++ 게임 스코어 조회.sql
@@ -4,12 +4,24 @@
 select * from game;
 
 insert into game(MEMBER_ID, GAME_SCORE)
-values('admin', 10);
+values('test', 8);
 
 update game
-set MEMBER_ID='admin', GAME_SCORE=11;
+set MEMBER_ID='admin', GAME_SCORE=10;
 
 commit;
+rollback;
+
+update game
+set MEMBER_ID='admin', GAME_SCORE=11
+where MEMBER_ID='admin' and game_score > (select game_score
+from game
+where member_id='admin');
+
+select game_score
+from game
+where member_id='admin';
+
 
 
 
