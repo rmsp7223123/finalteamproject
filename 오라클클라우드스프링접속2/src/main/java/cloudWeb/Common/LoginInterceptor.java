33bb558020f4fc2633c2def9e900abeffb5ab0d6
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-08-21 18:58:15 +0900

    인터셉터를 사용하기위한 클래스 및 beans 추가,

---
+++ 오라클클라우드스프링접속2/src/main/java/cloudWeb/Common/LoginInterceptor.java
@@ -0,0 +1,34 @@
+package cloudWeb.Common;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LoginInterceptor extends HandlerInterceptorAdapter{
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// 컨트롤러보다 먼저 수행되는 메소드
+		return super.preHandle(request, response, handler);
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// 컨트롤러가 수해오디고 화면이 보여지기 직전에 수행되는 메소드
+		super.postHandle(request, response, handler, modelAndView);
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+		super.afterCompletion(request, response, handler, ex);
+	}
+	
+	
+
+}
--- 오라클클라우드스프링접속2/src/main/java/com/hanul/cloudWeb/HomeController.java
+++ 오라클클라우드스프링접속2/src/main/java/com/hanul/cloudWeb/HomeController.java
@@ -1,5 +1,7 @@
 package com.hanul.cloudWeb;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.ibatis.session.SqlSession;
@@ -8,11 +10,13 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import cloudWeb.member.MemberVO;
 
 @Controller
-public class HomeController {
+public class HomeController implements HandlerInterceptor{
 
 	@Autowired
 	@Qualifier("project")
@@ -35,7 +39,32 @@ public class HomeController {
 		model.addAttribute("admin", admin);
 		return "default/login/loginCheck";
 	}
+
 	
 	
+	
+	
+	
+	
+	
+	
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		return false;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		
+	}
 
 }
