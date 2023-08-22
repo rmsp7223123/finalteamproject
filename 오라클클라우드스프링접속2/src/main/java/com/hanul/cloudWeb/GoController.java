9647a4bc8e23d540703cffb11076bdbff20c7732
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-08-22 17:19:05 +0900

    캘린더 리소스추가

--- 오라클클라우드스프링접속2/src/main/java/com/hanul/cloudWeb/GoController.java
+++ 오라클클라우드스프링접속2/src/main/java/com/hanul/cloudWeb/GoController.java
@@ -1,8 +1,12 @@
 package com.hanul.cloudWeb;
 
+import java.time.LocalDateTime;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class GoController {
 	
 	@RequestMapping("/home")
-	public String home(HttpSession session) {
+	public String home(HttpSession session, Model model, HttpServletRequest request, LocalDateTime localDateTime) {
 		session.setAttribute("category", "go");
 		return "go/home";
 	}
--- 오라클클라우드스프링접속2/src/main/webapp/WEB-INF/views/go/home.jsp
+++ 오라클클라우드스프링접속2/src/main/webapp/WEB-INF/views/go/home.jsp
@@ -8,6 +8,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-<h1>고독사확인용</h1>
+
 </body>
 </html>
