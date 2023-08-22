a9d590d4f580e897be3ed07e20192d59337edbb0
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-08-22 15:16:05 +0900

    안드 알람설정 바꿀때 삭제하는부분 수정

--- and/app/src/main/java/com/example/finalteamproject/setting/ChangeAlarmActivity.java
+++ and/app/src/main/java/com/example/finalteamproject/setting/ChangeAlarmActivity.java
@@ -38,41 +38,46 @@ public class ChangeAlarmActivity extends AppCompatActivity {
         CommonConn conn = new CommonConn(this, "setting/viewOption");
         conn.addParamMap("member_id", CommonVar.logininfo.getMember_id());
         conn.onExcute((isResult, data) -> {
-            ArrayList<OptionVO> list = new Gson().fromJson(data, new TypeToken<ArrayList<OptionVO>>(){}.getType());
+            ArrayList<OptionVO> list = new Gson().fromJson(data, new TypeToken<ArrayList<OptionVO>>() {
+            }.getType());
             SharedPreferences sharedPreferences = getSharedPreferences("NotificationPrefs", Context.MODE_PRIVATE);
             SharedPreferences.Editor editor = sharedPreferences.edit();
 
-            if(list.get(0).getOption_alarm().equals("N")) {
+            if (list.get(0).getOption_alarm().equals("N")) {
                 binding.tvCallMessageAlarmChange.setText("꺼짐");
                 // 기본 알람을 껐을때 알람창 데이터 삭제 추가
                 CommonConn conn1 = new CommonConn(this, "main/deleteAlarm");
                 conn1.addParamMap("receive_id", CommonVar.logininfo.getMember_id());
+                conn1.addParamMap("nickname", "");
+                conn1.addParamMap("alarm_content2", "");
                 conn1.onExcute((isResult1, data1) -> {
                     // 알람을 보냈을때 받는사람의 id가 알람이 N인 상태이면 보낼때 받는사람의 알람상태를 조회후 Y인 경우에만 보내게?
-                    FirebaseMessageReceiver.setIsEnabled(this,false);
+                    FirebaseMessageReceiver.setIsEnabled(this, false);
                     editor.putBoolean("notificationEnabled", isEnabled);
                     editor.apply();
                 });
             } else {
                 binding.tvCallMessageAlarmChange.setText("켜짐");
-                FirebaseMessageReceiver.setIsEnabled(this,true);
+                FirebaseMessageReceiver.setIsEnabled(this, true);
                 editor.putBoolean("notificationEnabled", isEnabled);
                 editor.apply();
             }
 
-            if(list.get(0).getOption_godok_alarm().equals("N")) {
+            if (list.get(0).getOption_godok_alarm().equals("N")) {
                 binding.tvGodokAlarmChange.setText("꺼짐");
-                // 기본 알람을 껐을때 알람창 데이터 삭제 추가
+                // 기본 알람을 껐을때 알람창 데이터가 있는경우 삭제 추가
                 CommonConn conn1 = new CommonConn(this, "main/deleteAlarm");
                 conn1.addParamMap("receive_id", CommonVar.logininfo.getMember_id());
+                conn1.addParamMap("nickname", "");
+                conn1.addParamMap("alarm_content2", "");
                 conn1.onExcute((isResult1, data1) -> {
-                    FirebaseMessageReceiver.setIsEnabled(this,false);
+//                    FirebaseMessageReceiver.setIsEnabled(this,false);
 //                    editor.putBoolean("notificationEnabled", isEnabled);
 //                    editor.apply();
                 });
             } else {
                 binding.tvGodokAlarmChange.setText("켜짐");
-                FirebaseMessageReceiver.setIsEnabled(this,true);
+//                FirebaseMessageReceiver.setIsEnabled(this,true);
 //                editor.putBoolean("notificationEnabled", isEnabled);
 //                editor.apply();
             }
@@ -81,9 +86,9 @@ public class ChangeAlarmActivity extends AppCompatActivity {
             CommonConn conn1 = new CommonConn(this, "setting/updateAlarm");
             conn1.addParamMap("member_id", CommonVar.logininfo.getMember_id());
             conn1.onExcute((isResult, data) -> {
-                if(binding.tvCallMessageAlarmChange.getText().toString().equals("꺼짐")) {
+                if (binding.tvCallMessageAlarmChange.getText().toString().equals("꺼짐")) {
                     binding.tvCallMessageAlarmChange.setText("켜짐");
-                }  else {
+                } else {
                     binding.tvCallMessageAlarmChange.setText("꺼짐");
                 }
             });
@@ -92,7 +97,7 @@ public class ChangeAlarmActivity extends AppCompatActivity {
             CommonConn conn1 = new CommonConn(this, "setting/updateGodokAlarm");
             conn1.addParamMap("member_id", CommonVar.logininfo.getMember_id());
             conn1.onExcute((isResult, data) -> {
-                if(binding.tvGodokAlarmChange.getText().toString().equals("꺼짐")) {
+                if (binding.tvGodokAlarmChange.getText().toString().equals("꺼짐")) {
                     binding.tvGodokAlarmChange.setText("켜짐");
                 } else {
                     binding.tvGodokAlarmChange.setText("꺼짐");
