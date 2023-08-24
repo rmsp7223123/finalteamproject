8b536d46aed51e213bf0311dec7f71cdf812fc85
615637 <129474590+615637@users.noreply.github.com>
2023-08-24 11:59:20 +0900

    location 정보 전송

--- and/app/src/main/java/com/example/finalteamproject/LocationService.java
+++ and/app/src/main/java/com/example/finalteamproject/LocationService.java
@@ -1,6 +1,5 @@
 package com.example.finalteamproject;
 
-import android.Manifest;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -9,7 +8,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
-import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
 import android.util.Log;
@@ -18,10 +16,13 @@ import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 
+import com.example.finalteamproject.common.CommonConn;
+import com.example.finalteamproject.common.CommonVar;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
 
 import java.security.Permission;
 
@@ -34,16 +35,15 @@ public class LocationService extends Service {
                 double latitude = locationResult.getLastLocation().getLatitude();
                 double longitude = locationResult.getLastLocation().getLongitude();
                 Log.v("LOCATION_UPDATE", latitude + ", " + longitude);
-                new Handler().postDelayed(()->{
-                    onStartCommand(new Intent(Constants.ACTION_START_LOCATION_SERVICE) , Service.START_FLAG_REDELIVERY , 0);
 
-                } , 4000);
+                CommonConn conn = new CommonConn(getApplicationContext(), "gps/location");
+                conn.addParamMap("member_id", CommonVar.logininfo.getMember_id());
+                conn.addParamMap("senior_latitude", latitude);
+                conn.addParamMap("senior_longitude", longitude);
+                conn.onExcute((isResult, data) -> {
 
-            }else{
-                new Handler().postDelayed(()->{
-                    onStartCommand(new Intent(Constants.ACTION_START_LOCATION_SERVICE) , Service.START_FLAG_REDELIVERY , 0);
+                });
 
-                } , 4000);
             }
         }
     };
@@ -55,33 +55,41 @@ public class LocationService extends Service {
     }
 
     private void startLocationService() {
-//        String channelId = "location_notification_channel";
-//        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-//        Intent resultIntent = new Intent();
-//        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, resultIntent, PendingIntent.FLAG_MUTABLE);
-//        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), channelId);
-//        builder.setSmallIcon(R.mipmap.ic_launcher);
-//        builder.setContentTitle("Location Service");
-//        builder.setDefaults(NotificationCompat.DEFAULT_ALL);
-//        builder.setContentText("Running");
-//        builder.setContentIntent(pendingIntent);
-//        builder.setAutoCancel(false);
-//        builder.setPriority(NotificationCompat.PRIORITY_MAX);
-//
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            if (notificationManager != null && notificationManager.getNotificationChannel(channelId) == null) {
-//                NotificationChannel notificationChannel = new NotificationChannel(channelId, "Location Service", NotificationManager.IMPORTANCE_HIGH);
-//                notificationChannel.setDescription("This channel is used by location service");
-//                notificationManager.createNotificationChannel(notificationChannel);
-//            }
-//        }
-//
-        LocationRequest locationRequest = LocationRequest.create();
-        locationRequest.setInterval(4000);
-        locationRequest.setFastestInterval(200);
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        String channelId = "location_notification_channel";
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        Intent resultIntent = new Intent();
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, resultIntent, PendingIntent.FLAG_MUTABLE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), channelId);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentTitle("위치 정보를 받아옵니다.");
+        builder.setDefaults(NotificationCompat.DEFAULT_ALL);
+        builder.setContentText("Running");
+        builder.setContentIntent(pendingIntent);
+        builder.setAutoCancel(false);
+        builder.setPriority(NotificationCompat.PRIORITY_MAX);
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if (notificationManager != null && notificationManager.getNotificationChannel(channelId) == null) {
+                NotificationChannel notificationChannel = new NotificationChannel(channelId, "Location Service", NotificationManager.IMPORTANCE_HIGH);
+                notificationChannel.setDescription("This channel is used by location service");
+                notificationManager.createNotificationChannel(notificationChannel);
+            }
+        }
+
+
+        // 1시간 간격으로 위치정보를 전송함.
+        LocationRequest.Builder locationBuilder = new LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY , 3600000);
+        locationBuilder.setMinUpdateIntervalMillis(3600000);
+        locationBuilder.setIntervalMillis(3600000);
+        locationBuilder.setMaxUpdateDelayMillis(10000);
+
+     //   LocationRequest req =()
+        LocationRequest locationRequest = locationBuilder.build();
+//        locationRequest.setInterval(4000);
+//        locationRequest.setFastestInterval(2000);
+//        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
@@ -92,18 +100,17 @@ public class LocationService extends Service {
             return;
         }
         LocationServices.getFusedLocationProviderClient(this).requestLocationUpdates(locationRequest, mLocationCallback, Looper.getMainLooper());
-
+        startForeground(Constants.LOCATION_SERVICE_ID, builder.build());
     }
 
     private void stopLocationService() {
         LocationServices.getFusedLocationProviderClient(this).removeLocationUpdates(mLocationCallback);
         stopForeground(true);
-    //    stopSelf();
+        stopSelf();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        super.onStartCommand(intent, Service.START_FLAG_REDELIVERY, startId);
         if (intent != null) {
             String action = intent.getAction();
             if (action != null) {
@@ -114,6 +121,6 @@ public class LocationService extends Service {
                 }
             }
         }
-        return START_STICKY;
+        return super.onStartCommand(intent, flags, startId);
     }
 }
