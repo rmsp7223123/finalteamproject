3ac3d46235799bd5ff547777a8c1d29170605497
sbn121 <129023074+sbn121@users.noreply.github.com>
2023-09-01 17:04:20 +0900

    위젯 일정

--- and/app/src/main/java/com/example/finalteamproject/main/CalendarVO.java
+++ and/app/src/main/java/com/example/finalteamproject/main/CalendarVO.java
@@ -1,6 +1,47 @@
 package com.example.finalteamproject.main;
 
+
 public class CalendarVO {
-	String member_id, calendar_content, calendar_date, calendar_importance;
-	int calendar_id;
+	private String member_id, calendar_content, calendar_date, calendar_importance;
+	private int calendar_id;
+
+	public String getMember_id() {
+		return member_id;
+	}
+
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
+	}
+
+	public String getCalendar_content() {
+		return calendar_content;
+	}
+
+	public void setCalendar_content(String calendar_content) {
+		this.calendar_content = calendar_content;
+	}
+
+	public String getCalendar_date() {
+		return calendar_date;
+	}
+
+	public void setCalendar_date(String calendar_date) {
+		this.calendar_date = calendar_date;
+	}
+
+	public String getCalendar_importance() {
+		return calendar_importance;
+	}
+
+	public void setCalendar_importance(String calendar_importance) {
+		this.calendar_importance = calendar_importance;
+	}
+
+	public int getCalendar_id() {
+		return calendar_id;
+	}
+
+	public void setCalendar_id(int calendar_id) {
+		this.calendar_id = calendar_id;
+	}
 }
